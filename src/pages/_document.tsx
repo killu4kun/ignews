b99e7873f17f0fba57cc24179740d760ffd5e6cf
killu4kun <mateeus.soarees@outlook.com>
2022-03-08 18:52:37 -0300

    header e nav bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto:wght@400;700;900&display=swap'
+            rel='stylesheet'
+          />
+          <link rel='shortcut icon' href='/favicon.png' type='image/png' />
         </Head>
         <body>
           <Main />
