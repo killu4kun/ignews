774edaf5f0e8ad54be8a024280c03fbd6a33f6c5
killu4kun <mateeus.soarees@outlook.com>
2022-03-09 15:26:58 -0300

    impl : subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,7 +8,21 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>
+            News about the<span> React</span> world
+          </h1>
+          <p>
+            Get access to all the publications
+            <br />
+            <span>for $9.99 month.</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src='/images/avatar.svg' alt='girl' />
+      </main>
     </>
   );
 }
