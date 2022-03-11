cb4ef846f7192a5d3d4dfabb87027d79a5c47113
killu4kun <mateeus.soarees@outlook.com>
2022-03-10 21:48:36 -0300

    startando backend

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,24 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  console.log(session);
+  return session ? (
     <button type='button' className={styles.signInButton}>
       <FaGithub color='#04d361' />
       Matheus Soares
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button
+      onClick={() => signIn('github')}
+      type='button'
+      className={styles.signInButton}
+    >
       <FaGithub color='#eba417' />
       Sign In
     </button>
