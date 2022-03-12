649f2f36cd55a0a0f62c7eb9ad2310a1ca62e364
killu4kun <mateeus.soarees@outlook.com>
2022-03-12 15:29:58 -0300

    stripe subscription e api axios

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export function SignInButton() {
-  const [session] = useSession();
+  const { data: session } = useSession();
 
   console.log(session);
   return session ? (
     <button type='button' className={styles.signInButton}>
       <FaGithub color='#04d361' />
-      Matheus Soares
-      <FiX color='#737380' className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
